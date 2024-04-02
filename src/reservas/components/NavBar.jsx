c962f4ff56da37a5393f25df729dc062e26fd06d
Jose Enrique Camacho Silvestre <enrique_silvestre28@outlook.com>
2024-04-01 22:04:21 -0400

    ambi-1: Removido comentarios del NavBar.

--- src/reservas/components/NavBar.jsx
+++ src/reservas/components/NavBar.jsx
@@ -9,16 +9,12 @@ const NavBar = ({anchoCaja}) => {
             }}>
         <Toolbar>
             <Grid container direction='column' justifyContent='center' alignItems='center' >
-                {/*<Typography variant='h6' noWrap component='div' marginTop={'20px'}>SISTEMA DE RESERVA DE</Typography>
-                <Typography variant='h6' noWrap component='div' marginBottom={'20px'}>AULAS FCYT</Typography>*/}
-
                 <Typography variant='h6' noWrap component='div' style={{ fontSize: '32.4px', marginTop: '1px', marginBottom: '0px' }}>
                 SISTEMA DE RESERVA DE
                 </Typography>
                 <Typography variant='h6' noWrap component='div' style={{ fontSize: '32.4px', marginBottom: '0px' }}>
                 AULAS FCYT
                 </Typography>
-
             </Grid>
         </Toolbar>
 
