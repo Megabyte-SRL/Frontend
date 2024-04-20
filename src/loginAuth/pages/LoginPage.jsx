9e052314a977fbe9ccb480e9c237d0053d0d7df6
IsrPort <isra.pqr@gmail.com>
2024-04-20 14:51:00 -0400

    Se agrego validaciones al formulario Inicio de Sesion(login)

--- src/loginAuth/pages/LoginPage.jsx
+++ src/loginAuth/pages/LoginPage.jsx
@@ -2,35 +2,70 @@ import { Link as RouterLink } from 'react-router-dom'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import React from 'react'
 import AuthLayout from '../layout/AuthLayout'
+import { useFormik } from 'formik'
+import * as Yup from "yup"
 
 const LoginPage = () => {
+
+  const formik = useFormik({
+    initialValues:{
+      email:"",
+      password:""
+    },
+
+    validationSchema: Yup.object({
+      email:Yup.string().email("Correo inválido").required("Campo requerido"),
+      password:Yup.string().required("Campo requerido").min(8,"Minimo 8 caracteres")
+    }),
+    validateOnChange:false,
+
+    onSubmit:(formValue) =>{
+      console.log("registro OK");
+      console.log(formValue);
+    }
+  })
+
+  console.log(formik.errors);
+
+
+
   return (
 
     <AuthLayout titulo='Login'>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
 
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
+              name='email'
               label="Correo"
-              type='email'
+              //type='email'
               placeholder='correo@gmail.com'
               fullWidth
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              error={formik.errors.email}
+              helperText={formik.errors.email}
             />
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
+              name='password'
               label="Contraseña"
               type='password'
               placeholder='Contraseña'
               fullWidth
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              error={formik.errors.password}
+              helperText={formik.errors.password}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mt: 2, mb: 2 }}>
             <Grid item xs={12} sm={12}>
-              <Button variant='contained' fullWidth>
+              <Button type='submit' variant='contained' fullWidth>
                 Login
               </Button>
 
