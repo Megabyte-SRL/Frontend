21bf6f14bcf32e335806840095073a79427aa622
Jose Enrique Camacho Silvestre <42417242+Enrequest@users.noreply.github.com>
2024-04-29 06:59:07 -0400

    Ambi/jose camacho/implementar funcionalidad para iniciar sesion (#28)
    * ambi: Implementado funcionalidad para iniciar y cerrar sesión.
    
    * ambi: Implementado validaciones de los campos de Login.
    
    * ambi: Implementada funcionalidad para validar contraseña y confirmar contraseña al registrar usuario.

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider } from './reservas/organisms/snackbarProvider/SnackbarProvider.jsx';
+import { AuthProvider } from './hooks/useAuth.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <SnackbarProvider>
-        <App />
-      </SnackbarProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <SnackbarProvider>
+          <App />
+        </SnackbarProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 )
--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import { Button, Grid, TextField } from '@mui/material';
-import { useFormik } from 'formik';
+import { Button, Grid } from '@mui/material';
+import { Form, Formik } from 'formik';
 import * as Yup from "yup";
 
 import CustomFormCard from '../../components/molecules/customFormCard/CustomFormCard';
+import { useAuth } from '../../hooks/useAuth';
+import { useSnackbar } from '../../reservas/organisms/snackbarProvider/SnackbarProvider';
+import { useNavigate } from 'react-router-dom';
+import CustomTextField from '../../components/atoms/customTextField/CustomTextField';
 
 const customStyles = {
   minHeight: '100vh',
@@ -13,68 +17,87 @@ const customStyles = {
 };
 
 const LoginPage = () => {
+  const { openSnackbar } = useSnackbar();
+  const auth = useAuth();
+  const navigate = useNavigate();
 
-  const formik = useFormik({
-    initialValues:{
-      email:"",
-      password:""
-    },
-
-    validationSchema: Yup.object({
-      email:Yup.string().email("Correo inválido").required("Campo requerido"),
-      password:Yup.string().required("Campo requerido").min(8,"Minimo 8 caracteres")
-    }),
-    validateOnChange:false,
-
-    onSubmit:(formValue) =>{
-      console.log("registro OK");
-      console.log(formValue);
-    }
-  })
-
-  console.log(formik.errors);
+  const validationSchema = Yup.object({
+    email:Yup.string().email("Correo inválido").required("Campo requerido"),
+    password:Yup.string().required("Campo requerido").min(8,"Minimo 8 caracteres")
+  });
 
   return (
     <CustomFormCard titulo='Login' styles={customStyles}>
-      <form onSubmit={formik.handleSubmit}>
-        <Grid container>
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField
-              name='email'
-              label="Correo"
-              //type='email'
-              placeholder='correo@gmail.com'
-              fullWidth
-              onChange={formik.handleChange}
-              value={formik.values.email}
-              error={formik.errors.email}
-              helperText={formik.errors.email}
-            />
-          </Grid>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={async (formValue, { resetForm }) => {
+          fetch(`${import.meta.env.VITE_LARAVEL_API_URL}/login`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formValue)
+          })
+            .then(async response => {
+              const { data } = await response.json();
+              auth.login(data.token, data.rol);
+              navigate('/dashboard');
+            })
+            .catch(error => {
+              console.log('error: ', error);
+              openSnackbar('Error al iniciar sesión', 'error');
+            })
+            .finally(() => {
+              resetForm();
+            });
+        }}
+      >
+        {({ errors, touched, isValid, dirty }) => (
+          <Form>
+            <Grid container>
+              <Grid item xs={12} sx={{ mt: 2 }}>
+                <CustomTextField
+                  name='email'
+                  type='email'
+                  label="Correo"
+                  placeholder='correo@gmail.com'
+                  touched={touched}
+                  errors={errors}
+                />
+              </Grid>
 
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField
-              name='password'
-              label="Contraseña"
-              type='password'
-              placeholder='Contraseña'
-              fullWidth
-              onChange={formik.handleChange}
-              value={formik.values.password}
-              error={formik.errors.password}
-              helperText={formik.errors.password}
-            />
-          </Grid>
+              <Grid item xs={12} sx={{ mt: 2 }}>
+                <CustomTextField
+                  name='password'
+                  type='password'
+                  label="Contraseña"
+                  placeholder='Contraseña'
+                  touched={touched}
+                  errors={errors}
+                />
+              </Grid>
 
-          <Grid container spacing={2} sx={{ mt: 2, mb: 2 }}>
-            <Grid item xs={12} sm={12}>
-              <Button type='submit' variant='contained' fullWidth>
-                Login
-              </Button>
+              <Grid container spacing={2} sx={{ mt: 2, mb: 2 }}>
+                <Grid item xs={12} sm={12}>
+                  <Button
+                    type='submit'
+                    color='primary'
+                    variant='contained'
+                    disabled={!isValid || !dirty}
+                    fullWidth
+                  >
+                    Login
+                  </Button>
+                </Grid>
+              </Grid>
             </Grid>
-          </Grid>
-        </Grid>
-      </form>
+          </Form>
+        )}
+      </Formik>
     </CustomFormCard>
   );
 }
