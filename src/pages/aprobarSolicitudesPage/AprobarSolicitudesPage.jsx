0f35f92028b14d2c176838aa54535fdd2ada411b
bran-deb <jairo.padilla.deb19@gmail.com>
2024-05-20 18:09:53 -0400

    update(AprobarSolicitud): add filter ambiente

--- src/pages/aprobarSolicitudesPage/AprobarSolicitudesPage.jsx
+++ src/pages/aprobarSolicitudesPage/AprobarSolicitudesPage.jsx
@@ -1,8 +1,11 @@
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TableSortLabel, Typography, Paper, Modal, Button, Grid } from '@mui/material'
-import { useState, useMemo, useEffect } from 'react'
-import { visuallyHidden } from '@mui/utils'
-import PropTypes from 'prop-types'
-import { useSnackbar } from '../../reservas/organisms/snackbarProvider/SnackbarProvider'
+import {
+    Box, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TableSortLabel, Typography, Paper, Modal, Button, Grid, TextField
+} from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
+import { useState, useMemo, useEffect } from 'react';
+import { visuallyHidden } from '@mui/utils';
+import PropTypes from 'prop-types';
+import { useSnackbar } from '../../reservas/organisms/snackbarProvider/SnackbarProvider';
 
 const createData = (id, fecha, ambiente, horario, capacidad, estado, prioridad) => ({
     id,
@@ -12,16 +15,16 @@ const createData = (id, fecha, ambiente, horario, capacidad, estado, prioridad) 
     capacidad,
     estado,
     prioridad,
-})
+});
 
 const rows = [
-    createData(1, '2023-05-16', 'Aula 101', '08:00 - 10:00', 30, 'Pendiente', 3),
-    createData(2, '2023-05-15', 'Aula 102', '10:00 - 12:00', 25, 'Pendiente', 2),
-    createData(3, '2023-05-16', 'Aula 103', '12:00 - 14:00', 20, 'Pendiente', 5),
-    createData(4, '2023-05-16', 'Aula 104', '14:00 - 16:00', 35, 'Rechazado', 1),
+    createData(1, '2023-05-15', 'Aula 101', '08:00 - 10:00', 30, 'Pendiente', 3),
+    createData(2, '2023-05-15', 'Aula 101', '10:00 - 12:00', 25, 'Aprobado', 2),
+    createData(3, '2023-05-16', 'Aula 103', '12:00 - 14:00', 20, 'Rechazado', 5),
+    createData(4, '2023-05-16', 'Aula 104', '14:00 - 16:00', 35, 'Pendiente', 1),
     createData(5, '2023-05-17', 'Aula 105', '16:00 - 18:00', 40, 'Aprobado', 4),
-    createData(6, '2023-05-18', 'Aula 106', '08:00 - 10:00', 30, 'Aprobado', 6),
-]
+    createData(6, '2023-05-18', 'Aula 106', '08:00 - 10:00', 30, 'Pendiente', 6),
+];
 
 const headCells = [
     { id: 'fecha', numeric: false, disablePadding: true, label: 'Fecha' },
@@ -30,33 +33,33 @@ const headCells = [
     { id: 'capacidad', numeric: true, disablePadding: false, label: 'Capacidad' },
     { id: 'prioridad', numeric: true, disablePadding: false, label: 'Prioridad' },
     { id: 'estado', numeric: false, disablePadding: false, label: 'Estado' },
-]
+];
 
 const descendingComparator = (a, b, orderBy) => {
-    if (b[orderBy] < a[orderBy]) return -1
-    if (b[orderBy] > a[orderBy]) return 1
-    return 0
-}
+    if (b[orderBy] < a[orderBy]) return -1;
+    if (b[orderBy] > a[orderBy]) return 1;
+    return 0;
+};
 
 const getComparator = (order, orderBy) =>
     order === 'desc'
         ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
 
 const stableSort = (array, comparator) => {
-    const stabilizedThis = array.map((el, index) => [el, index])
+    const stabilizedThis = array.map((el, index) => [el, index]);
     stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0])
-        if (order !== 0) return order
-        return a[1] - b[1]
-    })
-    return stabilizedThis.map((el) => el[0])
-}
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+};
 
 const EnhancedTableHead = ({ order, orderBy, onRequestSort }) => {
     const createSortHandler = (property) => (event) => {
-        onRequestSort(event, property)
-    }
+        onRequestSort(event, property);
+    };
 
     return (
         <TableHead>
@@ -74,33 +77,35 @@ const EnhancedTableHead = ({ order, orderBy, onRequestSort }) => {
                             onClick={createSortHandler(headCell.id)}
                         >
                             {headCell.label}
-                            {orderBy === headCell.id && (
+                            {orderBy === headCell.id ? (
                                 <Box component="span" sx={visuallyHidden}>
                                     {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                                 </Box>
-                            )}
+                            ) : null}
                         </TableSortLabel>
                     </TableCell>
                 ))}
             </TableRow>
         </TableHead>
-    )
-}
+    );
+};
 
 EnhancedTableHead.propTypes = {
     onRequestSort: PropTypes.func.isRequired,
     order: PropTypes.oneOf(['asc', 'desc']).isRequired,
     orderBy: PropTypes.string.isRequired,
-}
+};
 
 const AprobarSolicitudesPage = () => {
-    const [order, setOrder] = useState('asc')
-    const [orderBy, setOrderBy] = useState('fecha')
-    const [page, setPage] = useState(0)
-    const [rowsPerPage, setRowsPerPage] = useState(5)
-    const [open, setOpen] = useState(false)
-    const [selectedRow, setSelectedRow] = useState(null)
-    const [data, setData] = useState(rows)
+    const [order, setOrder] = useState('asc');
+    const [orderBy, setOrderBy] = useState('fecha');
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [open, setOpen] = useState(false);
+    const [selectedRow, setSelectedRow] = useState(null);
+    const [data, setData] = useState(rows);
+    const [filteredData, setFilteredData] = useState(rows);
+    const [searchValue, setSearchValue] = useState(null);
     const { openSnackbar } = useSnackbar();
 
     useEffect(() => {
@@ -108,7 +113,7 @@ const AprobarSolicitudesPage = () => {
             const updatedData = [...data];
             const ambientes = {};
 
-            // Agrupar por ambiente y ordenar por fecha
+            // Agrupar por ambiente
             updatedData.forEach(row => {
                 if (!ambientes[row.ambiente]) {
                     ambientes[row.ambiente] = [];
@@ -116,79 +121,96 @@ const AprobarSolicitudesPage = () => {
                 ambientes[row.ambiente].push(row);
             });
 
+            // Incrementar la prioridad solo para la primera fecha en cada grupo
             for (const ambiente in ambientes) {
-                if (ambientes[ambiente].length > 1) {
-                    // Ordenar por fecha
-                    ambientes[ambiente].sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
-                    // Aumentar prioridad de la primera fecha
-                    let firstDate = new Date(ambientes[ambiente][0].fecha);
-                    ambientes[ambiente].forEach(row => {
-                        if (new Date(row.fecha).getTime() === firstDate.getTime()) {
-                            row.prioridad += 1;
-                        }
+                if (ambientes[ambiente].length > 0) {
+                    // Encontrar la fila con la fecha más temprana
+                    let minDateRow = ambientes[ambiente].reduce((minRow, currentRow) => {
+                        return new Date(currentRow.fecha) < new Date(minRow.fecha) ? currentRow : minRow;
                     });
+                    // Incrementar la prioridad de la fila con la fecha más temprana
+                    minDateRow.prioridad += 1;
                 }
             }
 
             setData(updatedData);
+            setFilteredData(updatedData);
         };
 
         updatePriorities();
     }, []);
 
     const handleRequestSort = (event, property) => {
-        const isAsc = orderBy === property && order === 'asc'
-        setOrder(isAsc ? 'desc' : 'asc')
-        setOrderBy(property)
-    }
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
 
     const handleClick = (event, row) => {
-        setSelectedRow(row)
-        setOpen(true)
-    }
+        setSelectedRow(row);
+        setOpen(true);
+    };
 
     const handleClose = () => {
-        setOpen(false)
-        setSelectedRow(null)
-    }
+        setOpen(false);
+        setSelectedRow(null);
+    };
 
     const handleChangePage = (event, newPage) => {
-        setPage(newPage)
-    }
+        setPage(newPage);
+    };
 
     const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10))
-        setPage(0)
-    }
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
     const handleAccept = () => {
         const updatedData = data.map((row) =>
             row.id === selectedRow.id ? { ...row, estado: 'Aprobado' } : row
-        )
-        setData(updatedData)
-        handleClose()
+        );
+        setData(updatedData);
+        setFilteredData(updatedData.filter(row =>
+            !searchValue || row.ambiente.includes(searchValue)
+        ));
+        handleClose();
         openSnackbar('Solicitud aprobada exitosamente', 'success');
-    }
+    };
 
     const handleReject = () => {
         const updatedData = data.map((row) =>
             row.id === selectedRow.id ? { ...row, estado: 'Rechazado' } : row
-        )
-        setData(updatedData)
-        handleClose()
+        );
+        setData(updatedData);
+        setFilteredData(updatedData.filter(row =>
+            !searchValue || row.ambiente.includes(searchValue)
+        ));
+        handleClose();
         openSnackbar('Solicitud rechazada exitosamente', 'success');
-    }
+    };
 
-    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0
+    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filteredData.length) : 0;
 
     const visibleRows = useMemo(
         () =>
-            stableSort(data, getComparator(order, orderBy)).slice(
+            stableSort(filteredData, getComparator(order, orderBy)).slice(
                 page * rowsPerPage,
                 page * rowsPerPage + rowsPerPage
             ),
-        [order, orderBy, page, rowsPerPage, data]
-    )
+        [order, orderBy, page, rowsPerPage, filteredData]
+    );
+
+    const handleSearchChange = (event, newValue) => {
+        setSearchValue(newValue);
+        if (newValue) {
+            const filtered = data.filter(row =>
+                row.ambiente.includes(newValue)
+            );
+            setFilteredData(filtered);
+        } else {
+            setFilteredData(data);
+        }
+    };
 
     const modalBody = (
         <Box
@@ -239,7 +261,7 @@ const AprobarSolicitudesPage = () => {
                 </Button>
             </Box>
         </Box>
-    )
+    );
 
     return (
         <Grid container justifyContent="center">
@@ -264,6 +286,13 @@ const AprobarSolicitudesPage = () => {
                         <Typography variant="h4" align="center" gutterBottom>
                             Aprobar Solicitudes
                         </Typography>
+                        <Autocomplete
+                            options={[...new Set(data.map((row) => row.ambiente))]}
+                            value={searchValue}
+                            onChange={handleSearchChange}
+                            renderInput={(params) => <TextField {...params} label="Buscar por ambiente" variant="outlined" />}
+                            sx={{ mb: 2 }}
+                        />
                         <TableContainer>
                             <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle" size="medium">
                                 <EnhancedTableHead
@@ -295,7 +324,7 @@ const AprobarSolicitudesPage = () => {
                         <TablePagination
                             rowsPerPageOptions={[5, 10, 25]}
                             component="div"
-                            count={data.length}
+                            count={filteredData.length}
                             rowsPerPage={rowsPerPage}
                             page={page}
                             onPageChange={handleChangePage}
@@ -308,7 +337,7 @@ const AprobarSolicitudesPage = () => {
                 </Modal>
             </Grid>
         </Grid>
-    )
-}
+    );
+};
 
-export default AprobarSolicitudesPage
+export default AprobarSolicitudesPage;
