213ec93d977b8ee4fbabc336c6855165ac04dabe
IsrPort <146151940+IsrPort@users.noreply.github.com>
2024-05-23 08:54:20 -0400

    Administracion roles (#37)
    * Se agrego rutas para rol docente y administrador
    
    * jc/ambi: Arreglado errores de formato.
    
    ---------
    
    Co-authored-by: Jose Enrique Camacho Silvestre <enrique_silvestre28@outlook.com>

--- src/reservas/components/LateralBar.jsx
+++ src/reservas/components/LateralBar.jsx
@@ -4,10 +4,13 @@ import { Box, Collapse, Drawer, List, ListItemButton, ListItemText } from '@mui/
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
 
 const LateralBar = ({ anchoCaja = 240 }) => {
   const navigate = useNavigate();
   const [openAmbientesSubMenu, setOpenAmbientesSubMenu] = useState(false);
+
+  const auth = useAuth();
 
   const handleAmbientesClick = () => {
     if (openAmbientesSubMenu) {
@@ -26,58 +29,68 @@ const LateralBar = ({ anchoCaja = 240 }) => {
   }
 
   return (
-    <Box 
-        id='lateral-bar-box'
-        component='nav'
-        sx={{width:{sm:anchoCaja}, flexShrink:{sm:0}}}
+    <Box
+      id='lateral-bar-box'
+      component='nav'
+      sx={{ width: { sm: anchoCaja }, flexShrink: { sm: 0 } }}
     >
       <Drawer
         variant='permanent'
         open={true}
         sx={{
-          display: {xs: 'block'},
-           '& .MuiDrawer-paper': {boxSizing: 'border-box', width: anchoCaja, marginTop:13}
+          display: { xs: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: anchoCaja, marginTop: 13 }
         }}
       >
-        <List>
-          <ListItemButton id='crear-button' onClick={handleAmbientesClick}>
-            <ListItemText primary="Ambientes" />
-              {openAmbientesSubMenu ? <ExpandLess /> : <ExpandMore />}
-          </ListItemButton>
-          <Collapse in={openAmbientesSubMenu} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
-              <ListItemButton
-                id='crear-ambientes-button'
-                onClick={() => navigate('/dashboard/nuevo-ambiente')}
-                sx={{ pl: 4 }}
-              >
-                <ListItemText primary="Nuevo" />
+        {auth.rol != 'docente' ?
+          (
+            <List>
+              <ListItemButton id='crear-button' onClick={handleAmbientesClick}>
+                <ListItemText primary="Ambientes" />
+                {openAmbientesSubMenu ? <ExpandLess /> : <ExpandMore />}
               </ListItemButton>
-              <ListItemButton
-                id='carga-masiva-button'
-                onClick={() => navigate('/dashboard/carga-masiva')}
-                sx={{ pl: 4 }}
-              >
-                <ListItemText primary="Masiva" />
+              <Collapse in={openAmbientesSubMenu} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton
+                    id='crear-ambientes-button'
+                    onClick={() => navigate('/dashboard/nuevo-ambiente')}
+                    sx={{ pl: 4 }}
+                  >
+                    <ListItemText primary="Nuevo" />
+                  </ListItemButton>
+                  <ListItemButton
+                    id='carga-masiva-button'
+                    onClick={() => navigate('/dashboard/carga-masiva')}
+                    sx={{ pl: 4 }}
+                  >
+                    <ListItemText primary="Masiva" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+              <ListItemButton id='reserva-solicitudes' onClick={() => handleOtherItemClick('/dashboard/solicitudes')}>
+                <ListItemText primary='Crear solicitud ambiente' />
+              </ListItemButton>
+              <ListItemButton id='verificar-solicitudes' onClick={() => handleOtherItemClick('/dashboard/verificar-solicitudes')}>
+                <ListItemText primary='Verificar solicitudes' />
+              </ListItemButton>
+              <ListItemButton id='ver-button' onClick={() => handleOtherItemClick('/dashboard/visualizar-horario')}>
+                <ListItemText primary="Habilitar Fecha" />
+              </ListItemButton>
+              <ListItemButton id='registrar-usuario-button' onClick={() => handleOtherItemClick('/dashboard/signup')}>
+                <ListItemText primary="Usuarios" />
+              </ListItemButton>
+              <ListItemButton id='carga-masiva-docentes-button' onClick={() => handleOtherItemClick('/dashboard/carga-masiva-docentes')}>
+                <ListItemText primary="Carga masiva docentes" />
               </ListItemButton>
             </List>
-          </Collapse>
-          <ListItemButton id='reserva-solicitudes' onClick={() => handleOtherItemClick('/dashboard/solicitudes')}>
-            <ListItemText primary='Crear solicitud ambiente' />
-          </ListItemButton>
-          <ListItemButton id='verificar-solicitudes' onClick={() => handleOtherItemClick('/dashboard/verificar-solicitudes')}>
-            <ListItemText primary='Verificar solicitudes' />
-          </ListItemButton>
-          <ListItemButton id='ver-button' onClick={() => handleOtherItemClick('/dashboard/visualizar-horario')}>
-            <ListItemText primary="Habilitar Fecha" />
-          </ListItemButton>
-          <ListItemButton id='registrar-usuario-button' onClick={() => handleOtherItemClick('/dashboard/signup')}>
-            <ListItemText primary="Usuarios" />
-          </ListItemButton>
-          <ListItemButton id='carga-masiva-docentes-button' onClick={() => handleOtherItemClick('/dashboard/carga-masiva-docentes')}>
-            <ListItemText primary="Carga masiva docentes" />
-          </ListItemButton>
-        </List>
+          ) : (
+            <List>
+              <ListItemButton id='reserva-solicitudes' onClick={() => handleOtherItemClick('/solicitudDocente/solicitudes')}>
+                <ListItemText primary='Crear solicitud ambiente' />
+              </ListItemButton>
+            </List>
+          )
+        }
       </Drawer>
     </Box>
   );
