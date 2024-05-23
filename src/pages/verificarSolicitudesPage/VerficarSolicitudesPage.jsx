686d5dab503fd2afc3dd9170b92d3d0cbe15984f
Jose Enrique Camacho Silvestre <42417242+Enrequest@users.noreply.github.com>
2024-05-23 19:43:22 -0400

    jc/ambi: Implementado funcionalidad para aprobar solicitud de reserva de ambiente. (#42)

--- src/pages/verificarSolicitudesPage/VerficarSolicitudesPage.jsx
+++ src/pages/verificarSolicitudesPage/VerficarSolicitudesPage.jsx
@@ -1,58 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Box, Button, Grid, Paper, Typography } from '@mui/material';
+import { Box, Grid, Paper, Typography } from '@mui/material';
 import CustomTable from '../../components/organisms/customTable/CustomTable';
+import { useSnackbar } from '../../reservas/organisms/snackbarProvider/SnackbarProvider';
+import CustomModal from '../../components/organisms/customModal/CustomModal';
+import InformationVerificarSolicitudForm from '../../components/molecules/informacionVerificarSolicitudForm/InformationVerificarSolicitudForm';
 
 const VerficarSolicitudesPage = () => {
   const columns = [
     { id: 'fecha', label: 'Fecha'},
+    { id: 'fechaSolicitud', label: 'Fecha solicitud'},
     { id: 'ambiente', label: 'Ambiente'},
     { id: 'horario', label: 'Horario'},
     { id: 'capacidadAmbiente', label: 'Capacidad ambiente'},
     { id: 'capacidadReserva', label: 'Capacidad reserva' },
     { id: 'prioridad', label: 'Prioridad' },
-    {
-      id: 'acciones',
-      label: 'Acciones',
-      render: (row) => (
-        <>
-          <Button
-            color='primary'
-            variant='contained'
-          >
-            Aceptar
-          </Button>
-          <Button
-            variant='contained'
-            color='secondary'
-          >
-            Rechazar
-          </Button>
-        </>
-      )
-    }
   ];
 
-  const rows = [
-    {
-      id: 1,
-      fecha: '2023-01-01',
-      ambiente: 'Auditorio',
-      horario: '8:00 - 9:45',
-      capacidadAmbiente: 150,
-      capacidadReserva: 100,
-      prioridad: 1,
-    },
-    {
-      id: 1,
-      fecha: '2023-01-01',
-      ambiente: 'Auditorio',
-      horario: '9:45 - 11:15',
-      capacidadAmbiente: 150,
-      capacidadReserva: 50,
-      prioridad: 1,
-    }
-  ];
+  const { openSnackbar } = useSnackbar();
+  const [solicitudes, setSolicitudes] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedRow, setSelectedRow] = useState(null);
+
+  useEffect(() => {
+    obtenerListaSolicitudes();
+  }, []);
+
+  const obtenerListaSolicitudes = async () => {
+    fetch(`${import.meta.env.VITE_LARAVEL_API_URL}/list/solicitudesAmbientes`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al obtener la lista de horarios disponibles');
+        }
+        return response.json();
+      })
+      .then(({ data }) => {
+        setSolicitudes(data);
+      })
+      .catch(({ msg }) => {
+        console.error(msg);
+      });
+  };
+
+  const handleOnSubmitReserva = async (solicitudId) => {
+    fetch(`${import.meta.env.VITE_LARAVEL_API_URL}/reservarAmbiente/${solicitudId}`, {
+      method: 'POST',
+    })
+      .then(async response => {
+        const data = await response.json();
+        console.log('Registrar solicitud ambiente response: ', data);
+        openSnackbar('Solicitud registrado exitosamente', 'success');
+        obtenerListaSolicitudes();
+        setOpenModal(false);
+      })
+      .catch(async error => {
+        openSnackbar('Error al reservar ambiente', 'error');
+      })
+  };
+
+  const handleOpenReservaForm = (row) => {
+    setSelectedRow(solicitudes.find(solicitud => solicitud.id === row.id));
+    setOpenModal(true);
+  };
 
   return (
     <Grid container justifyContent='center'>
@@ -85,9 +94,31 @@ const VerficarSolicitudesPage = () => {
             
             <CustomTable
               columns={columns}
-              rows={rows}
-              onClickRow={(row) => console.log(row)}
+              rows={solicitudes.map(
+                solicitud => ({
+                  id: solicitud.id,
+                  fecha: solicitud.horarioDisponible.fecha,
+                  fechaSolicitud: solicitud.fechaSolicitud,
+                  ambiente: solicitud.horarioDisponible.ambiente,
+                  horario: solicitud.horarioDisponible.horario,
+                  capacidadAmbiente: solicitud.horarioDisponible.capacidad,
+                  capacidadReserva: solicitud.capacidad,
+                  prioridad: solicitud.prioridad
+                })
+              )}
+              onClickRow={(row) => handleOpenReservaForm(row)}
             />
+            <CustomModal
+              open={openModal}
+              onClose={() => setOpenModal(false)}
+              title='Detalles de la solicitud'
+            >
+              <InformationVerificarSolicitudForm
+                row={selectedRow}
+                onClose={() => setOpenModal(false)}
+                onSubmit={handleOnSubmitReserva}
+              />
+            </CustomModal>
           </Paper>
         </Box>
       </Grid>
