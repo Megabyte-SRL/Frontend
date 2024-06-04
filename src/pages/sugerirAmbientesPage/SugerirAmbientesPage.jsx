5a76704608b82a86aff867543c25543bbf0aa2ed
Jhosep2001-taku <josefranklim2001@gmail.com>
2024-06-03 23:07:25 -0400

    habilitacion filtros y puesta en formato latino

--- src/pages/sugerirAmbientesPage/SugerirAmbientesPage.jsx
+++ src/pages/sugerirAmbientesPage/SugerirAmbientesPage.jsx
@@ -1,155 +1,262 @@
-import { Box, Grid, Paper, Typography, TextField, Button, FormControl, InputLabel, Select, MenuItem, Checkbox } from '@mui/material';
-import React, { useState } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Chip, Grid, Paper, Typography, TextField, FormControl, InputLabel, Select, MenuItem, Checkbox } from '@mui/material';
+import { green, red, yellow } from '@mui/material/colors';
 import { Field, Formik } from 'formik';
+import { DataGrid } from '@mui/x-data-grid';
+import SolicitarAmbienteForm from '../../components/molecules/solicitarAmbienteForm/SolicitarAmbienteForm';
+import CustomModal from '../../components/organisms/customModal/CustomModal';
+import { useSnackbar } from '../../reservas/organisms/snackbarProvider/SnackbarProvider';
+import useTable from '../../hooks/useTable';
+import moment from 'moment';
+
+const fetchHorariosDisponibles = async (params) => {
+  const query = new URLSearchParams(params).toString();
+  const response = await fetch(`${import.meta.env.VITE_LARAVEL_API_URL}/list/horariosDisponibles?${query}`, {
+    headers: {
+      'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+    }
+  });
+
+  if (!response.ok) throw new Error('Error al obtener la lista de solicitudes');
+  const data = await response.json();
+  return data;
+};
 
 const renderEstado = (params) => {
-    return (
-        <Button
-            variant="contained"
-            color="success"
-            size="small"
-            sx={{ textTransform: 'none' }}
-        >
-            {params.value}
-        </Button>
-    );
+  switch (params.value) {
+    case 'disponible':
+      return <Chip label={params.value} style={{ borderRadius: '10%', backgroundColor: "#01AB5E", color: 'white' }} />;
+    case 'solicitado':
+      return <Chip label={params.value} style={{ borderRadius: '10%', backgroundColor: yellow[500], color: 'black' }} />;
+    default:
+      return <Chip label={params.value} style={{ backgroundColor: red[500], color: 'white' }} />;
+  }
 };
 
 const SugerirAmbientesPage = () => {
-    const [selectedRows, setSelectedRows] = useState({});
-    const horas = ['6:45 - 8:15', '8:15 - 9:45', '9:45 - 11:15', '11:15 - 12:45', '12:45 - 14:15', '14:15 - 15:45', '15:45 - 17:15', '17:15 - 18:45', '18:45 - 20:15', '20:15 - 21:45'];
+  const [selectedRows, setSelectedRows] = useState({});
+  const [filteredData, setFilteredData] = useState([]);
+  const [fechaFilter, setFechaFilter] = useState('');
+  const [horaFilter, setHoraFilter] = useState([]);
+  const horas = ['6:45:00 - 8:15:00', '8:15:00 - 9:45:00', '9:45:00 - 11:15:00', '11:15:00 - 12:45:00', '12:45:00 - 14:15:00', '14:15:00 - 15:45:00', '15:45:00 - 17:15:00', '17:15:00 - 18:45:00', '18:45:00 - 20:15:00', '20:15:00 - 21:45:00'];
 
-    const handleCheckboxChange = (event, id) => {
-        setSelectedRows((prev) => ({
-            ...prev,
-            [id]: event.target.checked,
-        }));
-    };
+  const handleCheckboxChange = (event, id) => {
+    setSelectedRows((prev) => ({
+      ...prev,
+      [id]: event.target.checked,
+    }));
+  };
 
-    const columns = [
-        { field: 'id', headerName: 'Id', width: 10 },
-        { field: 'fecha', headerName: 'Fecha solicitud', width: 180 },
-        { field: 'ambiente', headerName: 'Ambiente', width: 180 },
-        { field: 'horario', headerName: 'Horario', width: 180 },
-        { field: 'capacidadAmbiente', headerName: 'Capacidad', width: 150 },
-        { field: 'estado', headerName: 'Estado', width: 180, renderCell: renderEstado },
-        {
-            field: 'seleccionar',
-            headerName: 'Seleccionar',
-            width: 100,
-            renderCell: (params) => (
-                <Checkbox
-                    checked={selectedRows[params.id] || false}
-                    onChange={(event) => handleCheckboxChange(event, params.id)}
-                />
-            ),
+  const columns = [
+    { field: 'id', headerName: 'Id', width: 70 },
+    {
+      field: 'fecha',
+      headerName: 'Fecha solicitud',
+      width: 180,
+      valueFormatter: (params) => moment(params.value).format('DD/MM/YYYY'),
+    },
+    { field: 'ambiente', headerName: 'Ambiente', width: 180 },
+    { field: 'horario', headerName: 'Horario', width: 180 },
+    { field: 'capacidad', headerName: 'Capacidad', width: 150 },
+    { field: 'estado', headerName: 'Estado', width: 180, renderCell: renderEstado },
+    {
+        field: 'fecha',
+        headerName: 'Fecha solicitud',
+        width: 180,
+        valueFormatter: (params) => {
+          const value = params.value;
+          if (moment(value, 'YYYY-MM-DD', true).isValid()) {
+            return moment(value).format('DD/MM/YYYY');
+          } else {
+            return value; // Si no es una fecha válida, se muestra el valor original
+          }
         },
-    ];
+      },
+  ];
 
-    const rows = [
-        { id: 1, fecha: '24-05-12', ambiente: 'Aula 101', horario: '08:00-10:00', capacidadAmbiente: 35, estado: 'disponible' },
-        { id: 2, fecha: '24-05-12', ambiente: 'Aula 201', horario: '08:00-10:00', capacidadAmbiente: 42, estado: 'disponible' },
-        { id: 3, fecha: '24-05-12', ambiente: 'Aula 301', horario: '08:00-10:00', capacidadAmbiente: 45, estado: 'disponible' },
-        { id: 4, fecha: '24-05-12', ambiente: 'Aula 401', horario: '08:00-10:00', capacidadAmbiente: 16, estado: 'disponible' },
-        { id: 5, fecha: '24-05-12', ambiente: 'Aula 501', horario: '08:00-10:00', capacidadAmbiente: 205, estado: 'disponible' },
-        { id: 6, fecha: '24-05-12', ambiente: 'Aula 601', horario: '08:00-10:00', capacidadAmbiente: 150, estado: 'disponible' },
-        { id: 7, fecha: '24-05-5', ambiente: 'Aula 701', horario: '08:00-10:00', capacidadAmbiente: 44, estado: 'disponible' },
-        { id: 8, fecha: '24-05-12', ambiente: 'Aula 801', horario: '08:00-10:00', capacidadAmbiente: 36, estado: 'disponible' },
-        { id: 9, fecha: '24-05-12', ambiente: 'Aula 901', horario: '08:00-10:00', capacidadAmbiente: 65, estado: 'disponible' },
-    ];
+  const { openSnackbar } = useSnackbar();
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedRow, setSelectedRow] = useState({
+    id: 0,
+    fecha: '',
+    ambiente: '',
+    horario: '',
+    capacidad: 0,
+    estado: ''
+  });
 
-    return (
-        <Formik>
-            <Grid container justifyContent='center'>
-                <Grid item xs={12} md={8} lg={90}>
-                    <Box
-                        id='verificar-solicitudes-box'
-                        sx={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            marginTop: '5%',
-                            minHeight: 'calc(100vh - 20px)',
-                        }}
-                    >
-                        <Paper
-                            sx={{
-                                boxShadow: '0px 0px 10px 2px rgba(0,0,0,0.2)',
-                                padding: '2%',
-                                width: '100%',
-                                backgroundColor: '#F3F6F9',
-                            }}
-                        >
-                            <Typography variant='h4' align='center' gutterBottom>
-                                Ambientes disponibles
-                            </Typography>
+  const {
+    data,
+    searchText,
+    handleSearchChange,
+    filters,
+    handleFilterChange,
+    order,
+    orderBy,
+    handleSort,
+    rowsPerPage,
+    page,
+    handlePageChange,
+    handleRowsPerPageChange,
+    totalRows
+  } = useTable(fetchHorariosDisponibles, 'asc', 'fecha', { estado: 'disponible' });
 
-                            <Grid container spacing={2}>
-                                <Grid item xs={5} sx={{ mt: 2, ml:5 }}>
-                                    <Field
-                                        as={TextField}
-                                        name='fecha'
-                                        type='date'
-                                        label='Fecha'
-                                        variant='outlined'
-                                        InputLabelProps={{ shrink: true }}
-                                        width='50%'
-                                    />
-                                </Grid>
+  useEffect(() => {
+    const filtered = data
+      .filter(row => row.estado === 'disponible')
+      .filter(row => !fechaFilter || row.fecha === fechaFilter)
+      .filter(row => horaFilter.length === 0 || horaFilter.includes(row.horario));
+    const filteredWithIds = filtered.map((row, index) => ({ ...row, id: index + 1 }));
+    setFilteredData(filteredWithIds);
+  }, [data, fechaFilter, horaFilter]);
 
-                                <Grid item xs={2} sx={{ mt: 2,ml: -20 }}>
-                                    <FormControl fullWidth>
-                                        <InputLabel>Horas</InputLabel>
-                                        <Field
-                                            as={Select}
-                                            name='hora'
-                                            label='Hora'
-                                            multiple
-                                            value={[]}
-                                        >
-                                            {horas.map((hora) => (
-                                                <MenuItem key={hora} value={hora}>
-                                                    {hora}
-                                                </MenuItem>
-                                            ))}
-                                        </Field>
-                                    </FormControl>
-                                </Grid>
+  const handleOnSubmitSolicitud = async (values) => {
+    const [, grupoId] = values.grupo.split('-');
+    
+    fetch(`${import.meta.env.VITE_LARAVEL_API_URL}/solicitudesAmbientes`, {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + sessionStorage.getItem("token"),
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        horarioDisponibleId: values.id,
+        grupoId: grupoId,
+        capacidad: values.capacidad,
+        tipoReserva: values.tipoReserva,
+        docentes: values.docentes
+      })
+    })
+      .then(async response => {
+        const data = await response.json();
+        openSnackbar('Solicitud registrada exitosamente', 'success');
+        fetchHorariosDisponibles();
+        setOpenModal(false);
+      })
+      .catch(async error => {
+        openSnackbar('Error al registrar horario', 'error');
+      })
+  };
 
-                                <Grid item xs={11} sx={{ mt: 2 }}>
-                                    <Box sx={{ width: '95%' }}>
-                                        <DataGrid
-                                            rows={rows}
-                                            columns={columns}
-                                            initialState={{
-                                                pagination: {
-                                                    paginationModel: { page: 0, pageSize: 5 },
-                                                },
-                                            }}
-                                            pageSizeOptions={[5, 10]}
-                                            checkboxSelection={false}
-                                            autoHeight
-                                        />
-                                    </Box>
-                                </Grid>
+  const handleOpenSolicitationForm = (row) => {
+    setSelectedRow(row);
+    setOpenModal(true);
+  };
 
-                                <Grid item xs={12} sx={{ mt: 2, display: 'flex', justifyContent: 'flex-end' }}>
-                                    <Button
-                                        type='submit'
-                                        color='primary'
-                                        variant='contained'
-                                    >
-                                        Enviar Sugerencia
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </Paper>
-                    </Box>
+  const handleFechaFilterChange = (event) => {
+    setFechaFilter(event.target.value);
+  };
+
+  const handleHoraFilterChange = (event) => {
+    setHoraFilter(event.target.value);
+  };
+
+  return (
+    <Formik>
+      <Grid container justifyContent='center'>
+        <Grid item xs={10} md={12} lg={11}>
+          <Box
+            id='verificar-solicitudes-box'
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: '5%',
+              minHeight: '90%',
+              backgroundColor: '#f5f5f5',
+            }}
+          >
+            <Paper
+              sx={{
+                boxShadow: '0px 4px 10px rgba(0,0,0,0.1)',
+                padding: '2rem',
+                width: '100%',
+                backgroundColor: '#fff',
+                borderRadius: '8px'
+              }}
+            >
+              <Typography variant='h4' align='center' gutterBottom>
+                Ambientes Disponibles
+              </Typography>
+
+              <Grid container spacing={2}>
+                <Grid item xs={2} sx={{ mt: 2, ml: 5 }}>
+                  <TextField
+                    name='fecha'
+                    type='date'
+                    label='Fecha'
+                    variant='outlined'
+                    InputLabelProps={{ shrink: true }}
+                    fullWidth
+                    value={fechaFilter}
+                    onChange={handleFechaFilterChange}
+                  />
                 </Grid>
-            </Grid>
-        </Formik>
-    );
-};
+
+                <Grid item xs={2.3} sx={{ mt: 2, ml: 25 }}>
+                  <FormControl fullWidth>
+                    <InputLabel>Horas</InputLabel>
+                    <Select
+                      name='hora'
+                      label='Hora'
+                      multiple
+                      value={horaFilter}
+                      onChange={handleHoraFilterChange}
+                    >
+                      {horas.map((hora) => (
+                        <MenuItem key={hora} value={hora}>
+                          {hora}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Grid>
+
+                <Grid item xs={12} sx={{ mt: 2 }}>
+                  <Box sx={{ width: '100%' }}>
+                    <DataGrid
+                      rows={filteredData}
+                      columns={columns}
+                      initialState={{
+                        pagination: {
+                          paginationModel: { page: 0, pageSize: 5 },
+                        },
+                      }}
+                      pageSizeOptions={[5, 10]}
+                      checkboxSelection={false}
+                      autoHeight
+                    />
+                  </Box>
+                </Grid>
+
+                <Grid item xs={12} sx={{ mt: 2, display: 'flex', justifyContent: 'flex-end' }}>
+                  <Button
+                    type='submit'
+                    color='primary'
+                    variant='contained'
+                  >
+                    Enviar Sugerencia
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <CustomModal
+                open={openModal}
+                onClose={() => setOpenModal(false)}
+                title='Solicitar Ambiente'
+              >
+                <SolicitarAmbienteForm
+                  row={selectedRow}
+                  onClose={() => setOpenModal(false)}
+                  onSubmit={handleOnSubmitSolicitud}
+                />
+              </CustomModal>
+            </Paper>
+          </Box>
+        </Grid>
+      </Grid>
+    </Formik>
+  );
+}
 
 export default SugerirAmbientesPage;
